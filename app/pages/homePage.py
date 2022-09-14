1f63d520246802cfe359a5c7b1cedd2c7befce7b
Fatbrother <kevin92422@gmail.com>
2022-09-14 16:16:04 +0800

    addHomePage

--- app/pages/homePage.py
+++ app/pages/homePage.py
@@ -1,7 +1,14 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, render_template
+from app import db
 
 home = Blueprint('home', __name__)
 
 @home.route('/')
 def index():
-    return redirect(url_for('problemSet.index'))
+    problemSets = db.session.query(db.models.ProblemSet).all()
+    return render_template('home.html', problemSets=problemSets)
+
+@home.route('/<string:problemSetName>')
+def problemSetPage(problemSetName):
+    problemSet = db.session.query(db.models.ProblemSet).filter_by(name=problemSetName).first()
+    return render_template('problemSet.html', problemSet=problemSet)
