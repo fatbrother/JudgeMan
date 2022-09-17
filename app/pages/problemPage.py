4bcc254814576773865d0aedd055103b8c579463
LKV <a23202418@gmail.com>
2022-09-17 22:13:53 +0800

    use flask-sqlalchemy's method to query

--- app/pages/problemPage.py
+++ app/pages/problemPage.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template
-from app import db
+from app.database.models import Problem
 
 problem = Blueprint('problem', __name__)
 
 @problem.route('/problem/<int:problemId>')
 def index(problemId):
-    problem = db.session.query(db.models.Problem).filter_by(id=problemId).first()
+    problem = Problem.query().filter_by(id=problemId).first()
     return render_template('problem.html', problem=problem)
