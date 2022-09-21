24ccc9b673b4fc3138db74a8466d3c076e8c6247
Fatbrother <kevin92422@gmail.com>
2022-09-21 13:25:28 +0800

    remove float and advance page

--- app/pages/homePage.py
+++ app/pages/homePage.py
@@ -6,4 +6,5 @@ home = Blueprint('home', __name__)
 @home.route('/',  methods=['GET', 'POST'])
 def index():
     session['lastPage'] = url_for('home.index')
-    return render_template('home.html', problemSets=problemSets.viewAll())
+    # return render_template('home.html', problemSets=problemSets.viewAll())
+    return render_template('home.html', problemSets=problemSets.viewAll()[1:-1])
