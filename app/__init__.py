05fb6be2fe0c113898648d72628ac2ec214ce803
Xialai-Kulimi <isilkoodarze@gmail.com>
2022-10-05 23:52:42 +0800

    mv custom database_uri to .env

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, current_user
+from decouple import config
 
 
 def createApp() -> Flask:
     app = Flask(__name__, template_folder='../templates', static_folder='../static')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./site.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('SQLALCHEMY_DATABASE_URI') or 'sqlite:///./site.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = 'fatbrother0422issohandsome'
     return app
