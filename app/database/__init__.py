4977144386eab4b06de1b4b857ad1244a98943be
Xialai-Kulimi <isilkoodarze@gmail.com>
2022-10-05 22:41:26 +0800

    add annotation

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -3,8 +3,8 @@ from .initDB import init
 import os
 from pathlib import Path
 
-base = str(Path(os.path.dirname(os.path.abspath(__file__))).parent.absolute())
-if not Path(base, 'site.db').is_file():
+base = Path(os.path.dirname(os.path.abspath(__file__))).parent.absolute()
+if not Path(base, 'site.db').is_file():  # JudgeMan/app/site.db
     init()
 del(base)
 
