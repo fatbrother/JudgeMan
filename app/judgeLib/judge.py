f78ab2daeae232b230259357fb938129d9ab856d
Xialai-Kulimi <isilkoodarze@gmail.com>
2022-10-13 21:08:34 +0800

    Fix syntax error

--- app/judgeLib/judge.py
+++ app/judgeLib/judge.py
@@ -7,7 +7,7 @@ import os
 
 # this func return the result of judging and IO and expect of the program
 def judge(file_dir: str = None, code_text: str = None, language: str = None, input_dir: str = None, input: str = None, answer_dir: str = None, answer: str = None, timeLimit: float = 1, memoryLimit: int = 512) -> tuple[str, str | None, str | None, str | None]:
-    res = ''
+    res, input, output, answer = '', '', '', ''
 
     if file_dir is None:
         if language is None:
